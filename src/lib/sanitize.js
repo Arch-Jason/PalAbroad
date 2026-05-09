16096061454146fdb99b46543498d286c418bc05
Arch-Jason <jasonking393939@gmail.com>
2026-05-09 11:35:24 +0800

    posts+comments防xss

---
+++ src/lib/sanitize.js
@@ -0,0 +1,9 @@
+import { JSDOM } from 'jsdom';
+import DOMPurify from 'dompurify';
+
+const window = new JSDOM('').window;
+const purify = DOMPurify(window);
+
+export function sanitize(html) {
+  return purify.sanitize(html);
+}
